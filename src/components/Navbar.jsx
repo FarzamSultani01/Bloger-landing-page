ee9b4def068600b7868162a72c422e822c028bd1
FarzamSultani01 <7my6r9mqwn@privaterelay.appleid.com>
2026-04-19 22:32:50 +0430

    nav done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,27 +1,26 @@
-import React , {useState} from "react";
-import './Navbar.css'
-import logo from './logo.svg'
-import arrow from '../images/icon-arrow-light.svg'
-import Hamburger from '../images/icon-hamburger.svg'
-import Close from '../images/icon-close.svg'
+
+import React, { useState } from "react";
+import './Navbar.css';
+import logo from './logo.svg';
+import arrow from '../images/icon-arrow-light.svg';
+import Hamburger from '../images/icon-hamburger.svg';
+import Close from '../images/icon-close.svg';
+import arrowDark from '../images/icon-arrow-dark.svg';
 
 const Navbar = () => {
-    const [openMenu, setOpenMenu] = useState(null);
-    const toggleMenu = (menuName) => {
+  const [openMenu, setOpenMenu] = useState(null);
+  const toggleMenu = (menuName) => {
     setOpenMenu(openMenu === menuName ? null : menuName);
   };
+  const [hamburgerMenu, setHamburgerMenu] = useState(false);
 
   return (
     <nav className="nav__app"> 
-    <div className="nav__app-logo">
-    <img src={logo} alt="" />
-    </div>
-        <ul className="nav__app-links">
-         <li onClick={() => toggleMenu("product")}><a href="Product">Product <img src= {arrow} alt="" /></a></li>
-        <li><a href="Company">Company <img src= {arrow} alt="" /></a></li>
-        <li><a href="Connect">Connect <img src= {arrow} alt="" /></a></li>
-        </ul>
-        <ul className="nav__app-links">
+      <div className="nav__app-logo">
+        <img className="app__logo-bl" src={logo} alt="logo" />
+      </div>
+
+      <ul className="nav__app-links">
         <li onClick={() => toggleMenu("product")}>
           <a href="#">Product <img src={arrow} alt="" /></a>
           {openMenu === "product" && (
@@ -54,22 +53,61 @@ const Navbar = () => {
           )}
         </li>
       </ul>
-  <div className="auth">
-             <button className="login">Login</button>
+
+      <div className="auth">
+        <button className="login">Login</button>
         <button className="signup">Sign Up</button>
-        </div>   
-        <div className="app__navbar-smallscreen">
-          <img src={Hamburger} alt="pic" onClick={() => {}} />
-          <div className="app__navbar-smallscreen_overlay" >
-            <img src={Close} alt="" onClick={()=>{}} className="overlay__close"/>
-          <ul className="app__navbar-smallscreen-links">
-              <li><a href="Product">Product <img src= {arrow} alt="" /></a></li>
-              <li><a href="Company">Company <img src= {arrow} alt="" /></a></li>
-              <li><a href="Connect">Connect <img src= {arrow} alt="" /></a></li>
-          </ul>
+      </div>   
+
+     
+      <div className="app__navbar-smallscreen">
+        <img src={Hamburger} alt="menu" onClick={() => setHamburgerMenu(true)} />
+        {hamburgerMenu && (
+          <div className="app__navbar-smallscreen_overlay">
+             <div className="nav__app-logo-smallscreen">
+        <img className="logo__smallscreen" src={logo} alt="logo" />
+      </div>
+            <img src={Close} alt="close" onClick={() => setHamburgerMenu(false)} className="overlay__close"/>
+            <ul className="app__navbar-smallscreen-links">
+              <li onClick={()=>toggleMenu("product")}><a href="#">Product <img src={arrowDark} alt="" /></a>
+              {openMenu === "product" && (
+            <ul className="dropdown dropdown-smallscreen">
+              <li><a href="#">Overview</a></li>
+              <li><a href="#">Pricing</a></li>
+              <li><a href="#">Marketplace</a></li>
+            </ul>
+              )}
+              </li>
+              
+              <li onClick={()=> toggleMenu("company")}><a href="#">Company<img src={arrowDark} alt="" /></a>
+              {openMenu === "company" && (
+              <ul className="dropdown dropdown-smallscreen">
+               <li><a href="#">About</a></li>
+              <li><a href="#">Careers</a></li>
+              </ul>
+              )}
+              </li>
+              <li onClick={()=> toggleMenu("connect")}><a href="#">Connect<img src={arrowDark} alt="" /></a>
+              {openMenu === "connect" && (
+              <ul className="dropdown dropdown-smallscreen">  
+              <li><a href="#">Contact</a></li>
+              <li><a href="#">Newsletter</a></li>
+              <li><a href="#">LinkedIn</a></li>
+                </ul>
+                  )}
+              
+              </li>
+              
+              <div className="auth__smallscreen">
+                <button className="login">Login</button>
+                <button className="signup">Sign Up</button>
+              </div> 
+            </ul>
           </div>
-          </div>   
-     </nav>
-  )
-}
+        )}
+      </div>   
+    </nav>
+  );
+};
+
 export default Navbar;
